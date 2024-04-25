646a1eb5f85b473b7080752e60c4f8692c68feb9
Vaishnavi Rajput <162553453+rajput20012003@users.noreply.github.com>
2024-04-25 13:30:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
-    //Write your code here
+    // Get all <strong> elements in the paragraph
+    const boldElements = document.querySelectorAll('p strong');
 
-
+    // Change the color of each bold element to green
+    boldElements.forEach(element => {
+        element.style.color = 'green';
+    });
 }
 
-
 function return_normal() {
-    //Write your code here
+    // Get all <strong> elements in the paragraph
+    const boldElements = document.querySelectorAll('p strong');
 
-    
+    // Revert the color of each bold element back to black
+    boldElements.forEach(element => {
+        element.style.color = 'black';
+    });
 }
